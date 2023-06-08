3988b0ebbdd09cd9f4e81a5a396e732bc070edd2
James Welch <james.welch@cs.ox.ac.uk>
2023-06-08 06:05:55 +0100

    First working version

--- src/main/groovy/uk/ac/ox/softeng/maurodatamapper/iso11179/domain/helpers/MarshalHelper.groovy
+++ src/main/groovy/uk/ac/ox/softeng/maurodatamapper/iso11179/domain/helpers/MarshalHelper.groovy
@@ -8,16 +8,14 @@ import uk.ac.ox.softeng.maurodatamapper.iso11179.domain.MetadataBundle
 class MarshalHelper {
 
     static String marshallMetadataBundleToString(MetadataBundle metadataBundle, boolean prettyPrint = true) {
+        Marshaller marshaller = createMarshaller(prettyPrint)
+        StringWriter stringWriter = new StringWriter()
         try {
-            Marshaller marshaller = createMarshaller(prettyPrint)
-            StringWriter stringWriter = new StringWriter();
             marshaller.marshal(metadataBundle, stringWriter)
-            return stringWriter.toString()
-
         } catch (JAXBException e) {
             e.printStackTrace()
         }
-
+        return stringWriter.toString()
     }
 
     static void marshallMetadataBundleToFile(MetadataBundle metadataBundle, File file, boolean prettyPrint = true) {
@@ -34,7 +32,7 @@ class MarshalHelper {
     }
 
 
-    static private Marshaller createMarshaller(boolean prettyPrint = true) {
+    private static Marshaller createMarshaller(boolean prettyPrint = true) {
         JAXBContext context = JAXBContext.newInstance(MetadataBundle.class)
         Marshaller marshaller = context.createMarshaller()
         marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "https://www.iso.org/11179/-3/2012 iso11179-3_message.xsd")
